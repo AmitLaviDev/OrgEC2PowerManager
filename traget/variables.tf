6fd661babab5c54f193c7803a996f3c25ff3f4f9
Amit Lavi <Amit.Lavi@kyndryl.com>
2024-12-02 10:53:59 +0200

    🐛 FIX: formating tf files

--- traget/variables.tf
+++ traget/variables.tf
@@ -1,14 +1,14 @@
 variable "source_account_num" {
-  type = string
+  type        = string
   description = "AWS account number of the source account that will assume this role."
 }
 
 variable "region" {
-  type = string
+  type        = string
   description = "AWS region where resources are deployed."
 }
 
 variable "kms_key_id" {
-  type = string
+  type        = string
   description = "The ID of the KMS key to be used for grant access."
 }
